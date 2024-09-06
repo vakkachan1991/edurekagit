e50d2004d969088357fc1570e2f687a7b044c6c8
vakkachan1991 <68135031+vakkachan1991@users.noreply.github.com>
2024-09-06 02:49:26 -0700

    Update test_hello.py
    gy

--- test_hello.py
+++ test_hello.py
@@ -1,1 +1,9 @@
+import unittest
+from hello import say_hello
 
+class TestHello(unittest.TestCase):
+    def test_say_hello(self):
+        self.assertEqual(say_hello(), "Hello from Jenkins!")
+
+if _name_ == "_main_":
+    unittest.main()
